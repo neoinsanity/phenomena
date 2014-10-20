23d2d4f2bd120ddcf9b1f0ac0a6ceccb0439a7d4
neoinsanity <mindbender@gmail.com>
2014-10-20 16:16:28 -0700

    Extended configuration registration testing for default connections.

--- test/connection_manager_test.py
+++ test/connection_manager_test.py
@@ -17,38 +17,58 @@ class ConnectionManagerTest(unittest.TestCase):
 
         # validate listener
         self.assertIsNotNone(listener)
+        self.assertEqual('localhost', listener.address)
+        self.assertIs(long, type(listener.id))
+        self.assertEqual(60053, listener.port)
+        self.assertEqual('tcp', listener.protocol)
+        self.assertEqual('pull', listener.type)
 
         # validate core state
         # todo: raul - add the rest of the test as the method call us filled.
 
-    def test_register_requester(self):
+    def test_register_default_requester(self):
         # test subjects
         core = EventCore(command_port=63353)
         requester = core.connection_manager.register_requester()
 
         # validate requester
         self.assertIsNotNone(requester)
+        self.assertEqual('localhost', requester.address)
+        self.assertIs(long, type(requester.id))
+        self.assertEqual(60053, requester.port)
+        self.assertEqual('tcp', requester.protocol)
+        self.assertEqual('request', requester.type)
 
         # validate core state
         # todo: raul - add the rest of the test as the method call is filled.
 
-    def test_register_responder(self):
+    def test_register_default_responder(self):
         core = EventCore(command_port=63353)
         responder = core.connection_manager.register_responder()
 
         # validate responder
         self.assertIsNotNone(responder)
+        self.assertEqual('*', responder.address)
+        self.assertIs(long, type(responder.id))
+        self.assertEqual(60053, responder.port)
+        self.assertEqual('tcp', responder.protocol)
+        self.assertEqual('reply', responder.type)
 
         # validate core state
         # todo: raul - add the rest of the test as the method call is filled.
 
-    def test_register_sink(self):
+    def test_register_default_sink(self):
         # test subjects
         core = EventCore(command_port=63353)
         sink = core.connection_manager.register_sink()
 
         # validate sink
         self.assertIsNotNone(sink)
+        self.assertEqual('*', sink.address)
+        self.assertIs(long, type(sink.id))
+        self.assertEqual(60053, sink.port)
+        self.assertEqual('tcp', sink.protocol)
+        self.assertEqual('push', sink.type)
 
         # validate core state
         # todo: raul - add the rest of the test as the method call is filled.
