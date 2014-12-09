bca2bdd8a4e38ecf92a7525bd1c40a1234bfa92a
neoinsanity <mindbender@gmail.com>
2014-12-09 01:03:13 -0800

    Created dev version 0.0.1b.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ requires = [
 
 setup(
     name='phenomena',
-    version='0.0.1a',
+    version='0.0.1b',
     author='Raul Gonzalez',
     author_email='mindbender@gmail.com',
     url='https://github.com/neoinsanity/phenomena',
