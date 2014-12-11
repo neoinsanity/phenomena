c932bfb344c542faa383ba63becac86c98ee0743
neoinsanity <mindbender@gmail.com>
2014-12-11 00:59:54 -0800

    Set to version 0.0.1.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ requires = [
 
 setup(
     name='phenomena',
-    version='0.0.1b',
+    version='0.0.1',
     author='Raul Gonzalez',
     author_email='mindbender@gmail.com',
     url='https://github.com/neoinsanity/phenomena',
